9a834d7fb1e1439c94e6eb1a309f8c9ef9e1d93e
Filipe Oliveira <contato@fmoliveira.com.br>
2016-08-21 18:11:14 -0300

    Configuração inicial do Hapi.

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({
+  host: 'localhost',
+  port: 8000
+});
+
+server.route({
+  method: 'GET',
+  path: '/hello',
+  handler: (request, reply) => {
+    return reply('Hello world!');
+  }
+});
+
+server.start((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Server running at ${server.info.uri}`);
+});
