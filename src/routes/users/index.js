0c5201fd95d224909b56c4f6f28391cfb9bbf39a
Filipe Oliveira <contato@fmoliveira.com.br>
2016-08-28 16:43:19 -0300

    API para listar as páginas do usuário.

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -1,9 +1,13 @@
 import { UsersList } from './list';
 import { UsersLogin } from './login';
 import { UsersUpdate } from './update';
+import { UserMePagesList } from './user-me-pages';
+import { UserPagesList } from './user-pages';
 
 export const UsersRoutes = [
   UsersList,
   UsersLogin,
-  UsersUpdate
+  UsersUpdate,
+  UserMePagesList,
+  UserPagesList
 ]
