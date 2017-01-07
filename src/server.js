e8f7f03ee2adba20a9f4efe350c36c25b59cb9d6
Filipe Oliveira <contato@fmoliveira.com.br>
2017-01-06 23:50:34 -0200

    Validação de dados.

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import Hapi from 'hapi'
+import Joi from 'joi'
 
 const server = new Hapi.Server()
 server.connection({port: 4000})
@@ -6,6 +7,13 @@ server.connection({port: 4000})
 server.route({
   method: 'GET',
   path: '/hello/{name*}',
+  config: {
+    validate: {
+      params: {
+        name: Joi.string().min(2).max(40).alphanum().required()
+      }
+    }
+  },
   handler: (req, reply) => {
     reply(`Hello ${req.params.name}!`)
   }
